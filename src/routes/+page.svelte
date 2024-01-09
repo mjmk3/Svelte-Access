6c67c24745db145cac5ff0883f2edd60b67afe00
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-09 15:28:03 +0300

    Svelte Access

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,29 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Login from "./access/Login.svelte";
+    import Register from "./access/Register.svelte";
+    import Home from "./page/Home.svelte";
+    import Router, {link} from 'svelte-spa-router';
+
+    const routes = {
+        '/': Home,
+        '/login': Login,
+        '/register': Register,
+    };
+</script>
+
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" use:link class="nav-link px-2 text-white">Home</a></li>
+            </ul>
+
+            <div class="text-end">
+                <a href="/login" use:link class="btn btn-outline-light me-2">Login</a>
+                <a href="/register" use:link class="btn btn-outline-light me-2">Register</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<Router {routes}/>
